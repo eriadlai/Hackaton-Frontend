92a4c805fb33b9f682ed331e8fa27abb7c379566
Adlai CG <42752044+eriadlai@users.noreply.github.com>
2022-11-12 11:38:14 -0800

    test

--- principal/src/components/TextoInstrucciones.jsx
+++ principal/src/components/TextoInstrucciones.jsx
@@ -1,5 +1,3 @@
-
-
 import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
@@ -24,41 +22,6 @@ export default function TextoInstrucciones() {
         <TextField
           id="textoInstrucciones"
           label="Instrucciones"
-          multiline
-          rows={4}
-          defaultValue=""
-        />
-      </div>
-    </Box>
-  );
-}
-
-
-
-import * as React from "react";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-
-export default function TextoListaMateriales() {
-  const [value, setValue] = React.useState("Controlled");
-
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  };
-
-  return (
-    <Box
-      component="form"
-      sx={{
-        "& .MuiTextField-root": { maxwidth: "100%", width: 550 },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-        <TextField
-          id="textoListaMateriales"
-          label="Lista de Materiales"
           multiline
           rows={4}
           defaultValue=""
