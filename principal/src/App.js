f3276cf15593eac00edb6c509e6748613677c71c
Adlai CG <42752044+eriadlai@users.noreply.github.com>
2022-11-12 08:24:56 -0800

    Refactorizacion
    Refactorizacón del codigo, adicion y modulacion de carpetas y componentes.

--- principal/src/App.js
+++ principal/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import BarraSuperior from './components/BarraSuperior';
 import Dashboard from './components/Dashboard';
-import Login from './components/Login'
+import Login from './pages/Login'
 function App() {
     return ( 
         <>
--- principal/src/pages/Login.jsx
+++ principal/src/pages/Login.jsx
@@ -12,7 +12,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import "./Style.css";
+import "../index.css";
 import { useState } from "react";
 function Copyright(props) {
   return (
