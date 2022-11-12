cc76821d127147a9e1d89b31d70187275b1cd5ed
Adlai CG <42752044+eriadlai@users.noreply.github.com>
2022-11-12 13:20:48 -0800

    adicion Registro Funcional

--- principal/src/pages/SignIn.jsx
+++ principal/src/pages/SignIn.jsx
@@ -56,14 +56,20 @@ export default function SignUp() {
       oCorreo: oCorreo,
       oPassword: oPassword,
     };
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+    fetch(oAPI, {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify(oDatos),
-    };
-    fetch(oAPI, requestOptions)
+    })
       .then((response) => response.json())
-      .then((data) => this.setState({ postId: data.id }));
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
 
   const [mostrar, setMostrar] = useState(false);
