877e1309d6910b489c82dfcde6ccc0a5183cf21a
SofiAle04 <98795705+SofiAle04@users.noreply.github.com>
2022-11-12 02:31:08 -0800

    Hice menú y cambie los botones feos

--- principal/src/App.js
+++ principal/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Login from "./components/Login";
 import BotonLog from "./components/BotonLog";
-import BotonSignIn from "./components/BotonSignIn";
+import BarraSuperior from "./components/BarraSuperior";
 
 function App() {
   return (
     <>
+      <BarraSuperior />
       <Login />
       <BotonLog />
-      <BotonSignIn />
     </>
   );
 }
--- principal/src/components/botonLog.jsx
+++ principal/src/components/botonLog.jsx
@@ -3,19 +3,5 @@ import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 
 export default function BotonLog() {
-  return (
-    <Stack spacing={2} direction="row">
-      <Button
-        variant="text"
-        size="small"
-        onClick={() => {
-          alert("clicked");
-        }}
-        Click
-        me
-      >
-        Log in
-      </Button>
-    </Stack>
-  );
+  return <Button variant="outlined">Log In</Button>;
 }
