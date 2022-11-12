65df0a2b149b28c1955c44b98c91b73c754b6fe6
aaronSE11 <93004274+aaronSE11@users.noreply.github.com>
2022-11-12 00:28:14 -0800

    styles.css

--- principal/src/components/login.jsx
+++ principal/src/components/login.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-
+import "./Style.css";
 class Login extends Component {
     
     render() { 
         return (
             <>
             <Box sx={{ width: '100%', maxWidth: 500 }}>
-        <Typography variant="h3" gutterBottom>
+                 
+        <Typography variant="h4" className='negrita' gutterBottom>
         INICIAR SESION 
 
       </Typography>
