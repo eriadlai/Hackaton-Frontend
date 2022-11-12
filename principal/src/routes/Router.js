d4e4c5353ad1c706f735319d48c99815d5adec98
Adlai CG <42752044+eriadlai@users.noreply.github.com>
2022-11-12 14:03:11 -0800

    Updates-Integradas

--- principal/src/routes/Router.js
+++ principal/src/routes/Router.js
@@ -4,6 +4,8 @@ import Principal from "../pages/Principal";
 import SignIn from "../pages/SignIn";
 import Navbar from "../components/BarraSuperior";
 import PostIdea from "../pages/PostIdea";
+import RecetaUbicacion from "../pages/RecetaUbicacion";
+import Publicaciones from "../pages/Publicaciones";
 export default function Router() {
   return (
     <BrowserRouter>
@@ -13,6 +15,8 @@ export default function Router() {
         <Route path="/Login" element={<Login />} />
         <Route path="/SignIn" element={<SignIn />} />
         <Route path="/PostIdea" element={<PostIdea />} />
+        <Route path="/RecetaUbicacion" element={<RecetaUbicacion />} />
+        <Route path="/Publicaciones" element={<Publicaciones />} />
       </Routes>
     </BrowserRouter>
   );
