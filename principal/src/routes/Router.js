5fd0dadaf6b2653ee289aaee9e2eed5bda3c4044
Adlai CG <42752044+eriadlai@users.noreply.github.com>
2022-11-12 11:24:45 -0800

    PostIdea

--- principal/src/routes/Router.js
+++ principal/src/routes/Router.js
@@ -3,6 +3,7 @@ import Login from "../pages/Login";
 import Principal from "../pages/Principal";
 import SignIn from "../pages/SignIn";
 import Navbar from "../components/BarraSuperior";
+import PostIdea from "../pages/PostIdea";
 export default function Router() {
   return (
     <BrowserRouter>
@@ -11,6 +12,7 @@ export default function Router() {
         <Route path="/" element={<Principal />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/PostIdea" element={<PostIdea />} />
       </Routes>
     </BrowserRouter>
   );
