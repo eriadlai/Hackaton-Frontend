4e55932f0483738c21dddaf7335bdce261c57ebc
Adlai CG <42752044+eriadlai@users.noreply.github.com>
2022-11-12 08:08:14 -0800

    Feed de Categorias
    Adicion de componente FeedCategorias

--- principal/src/App.js
+++ principal/src/App.js
@@ -1,30 +1,13 @@
-<<<<<<< Updated upstream
 import React from 'react';
-import Botonplastico from './components/Botonplastico';
-
-function App() {
-    return ( 
-        <>
-         <Botonplastico/>
-        
-        </>
-    );
-=======
-import React from "react";
-import Login from "./components/Login";
-import BotonLog from "./components/BotonLog";
-import BotonSignIn from "./components/BotonSignIn";
-import BarraSuperior from "./components/BarraSuperior";
-import Principal from "./components/Principal";
-import Botonplastico from "./components/Botonplastico";
+import FeedCategorias from './components/FeedCategorias';
 
 function App() {
   return (
     <>
-      <Botonplastico/>
+      <FeedCategorias/>
+
     </>
   );
->>>>>>> Stashed changes
 }
 
 export default App;
---
+++ principal/src/components/FeedCategorias.jsx
@@ -0,0 +1,81 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions, Grid } from "@mui/material";
+const images = [
+  {
+    url: "madera.jpg",
+    title: "Madera",
+    description: "Descripcion del apartado de MADERA",
+    width: "25%",
+  },
+  {
+    url: "papel.jpg",
+    title: "Papel/Cartón",
+    description: "Descripcion del apartado de PAPEL",
+    width: "25%",
+  },
+  {
+    url: "vidrio.jpg",
+    title: "Vidirio",
+    description: "Descripcion del apartado de VIDRIO",
+    width: "25%",
+  },
+  {
+    url: "plastico.jpg",
+    title: "Plastico",
+    description: "Descripcion del apartado de PLASTICO",
+    width: "25%",
+  },
+  {
+    url: "metales.jpg",
+    title: "Metales",
+    description: "Descripcion del apartado de METALES",
+    width: "25%",
+  },
+  {
+    url: "textilws.jpg",
+    title: "Textiles",
+    description: "Descripcion del apartado de TEXTILES",
+    width: "25%",
+  },
+];
+export default function MultiActionAreaCard() {
+  return (
+    <Grid
+      container
+      spacing={{ xs: 2, md: 3 }}
+      columns={{ xs: 4, sm: 8, md: 12 }}
+    >
+      {images.map((image) => (
+        <Grid item xs={2} sm={4} md={4} key={image}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image={image.url}
+                alt={image.title}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {image.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {image.description}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary">
+                Ver mas
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
