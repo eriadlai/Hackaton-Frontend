e564c4c5def9d61379b623a7de875197811867be
Adlai CG <42752044+eriadlai@users.noreply.github.com>
2022-11-12 11:19:43 -0800

    Post en Progreso

--- principal/src/pages/SignIn.jsx
+++ principal/src/pages/SignIn.jsx
@@ -5,7 +5,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
@@ -14,6 +13,7 @@ import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "../components/Style.css";
 import { useState } from "react";
+const oAPI = "http://localhost:3001/API/Reciclaje/Usuario";
 function Copyright(props) {
   return (
     <Typography
@@ -32,13 +32,44 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function SignUp() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+  const [oNombre, setNombre] = useState("");
+  const nombreOnChange = (event) => {
+    setNombre(event.target.value);
+  };
+  const [oUsuario, setUsuario] = useState("");
+  const usuarioOnChange = (event) => {
+    setUsuario(event.target.value);
+  };
+  const [oCorreo, setCorreo] = useState("");
+  const correoOnChange = (event) => {
+    setCorreo(event.target.value);
+  };
+  const [oPassword, setPassword] = useState("");
+  const passwordOnChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    const oDatos = {
+      oNombre: oNombre,
+      oUsuario: oUsuario,
+      oCorreo: oCorreo,
+      oPassword: oPassword,
+    };
+    fetch(oAPI, {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(oDatos),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
 
   const [mostrar, setMostrar] = useState(false);
@@ -70,22 +101,26 @@ export default function SignUp() {
               <Grid item xs={12} sm={6}>
                 <TextField
                   autoComplete="given-name"
-                  name="firstName"
+                  name="nombre"
                   required
                   fullWidth
-                  id="firstName"
+                  id="nombre"
                   label="Nombre"
                   autoFocus
+                  value={oNombre}
+                  onChange={nombreOnChange}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField
                   required
                   fullWidth
-                  id="lastName"
-                  label="Apellido"
-                  name="Lastname"
+                  id="usuario"
+                  label="Usuario"
+                  name="usuario"
                   autoComplete="family-name"
+                  value={oUsuario}
+                  onChange={usuarioOnChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -96,6 +131,8 @@ export default function SignUp() {
                   label="Correo Electronico"
                   name="email"
                   autoComplete="email"
+                  value={oCorreo}
+                  onChange={correoOnChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -107,6 +144,8 @@ export default function SignUp() {
                   type={mostrar ? "text" : "password"}
                   id="password"
                   autoComplete="new-password"
+                  value={oPassword}
+                  onChange={passwordOnChange}
                 />
               </Grid>
               <Grid item xs={12}>
